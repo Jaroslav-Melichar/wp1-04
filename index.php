a3d2f074faf5d025a37b2b38cfb8b02824c5ad5a
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-03-23 11:38:46 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+$temperature = 2700
+
+if($temperature > 2700 ) {
+    echo "Zakoupil jste si AK47";
+ } elseif($temperature < 2700 ) {
+     echo "Nemáte dostatek peněz na AK47 tak jste si zakoupil Desert eagle a vestu";
+ } else {
+     echo "Nemáte dostatek peněz na jakoukoliv zbraň proto vám zůstane pouze nůž"
+ }
+
+
+
+
+</body>
+</html>
